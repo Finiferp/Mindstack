1de08de91c3dab62b30b999ca850a0f4b2957121
Daniel <daniel-batista22@hotmail.com>
2026-01-19 17:11:30 +0100

    changed name and logo and structure test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,9 +10,9 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Mindstack',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
